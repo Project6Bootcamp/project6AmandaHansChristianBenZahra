38d60fcb53f00166befcf64732aa74cad61dbbd7
Zahra <zahrapaliwalla@gmail.com>
2020-12-08 20:28:54 -0500

    stage commits

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
-import { Component } from 'react';
+import { Component, Fragment } from "react";
 
 class Header extends Component {
-    constructor() {
-        super();
-        this.state = {
-            headerText: '',
-            subheaderText: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      headerText: "",
+      subheaderText: "",
+    };
+  }
 
-    render() {
-        return (
-            <header>
-                <h1>{this.props.headerText}</h1>
-                <h2>{this.props.subheaderText}</h2>
-            </header>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <header>
+          <h1>{this.props.headerText}</h1>
+          <h2>{this.props.subheaderText}</h2>
+        </header>
+
+        <a className="btn-slice" href="#">
+          <div class="top">
+            <span>Sliced Button</span>
+          </div>
+          <div className="bottom">
+            <span>Sliced Button</span>
+          </div>
+        </a>
+      </Fragment>
+    );
+  }
 }
 
 export default Header;
