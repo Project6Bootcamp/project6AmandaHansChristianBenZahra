9c77f48b6db18adcffac85c6c5be2310d6356a35
HansChristian <hc.sev15@gmail.com>
2020-12-10 11:54:52 -0500

    Updated memeCreator form.

--- src/components/MemeCreator.js
+++ src/components/MemeCreator.js
@@ -6,20 +6,18 @@ class MemeCreator extends Component{
         super();
         this.state = {
             userMemes:'',
-            userTopText: 'Top Text',
-            userBottomText: 'Bottom Text'
+            userTopText: '',
+            userBottomText: ''
         }
     }
 
     handleInputChangeTopText = (e) => {
-        console.log(e.target.value);
         this.setState({
             userTopText: e.target.value
         })
     }
 
     handleInputChangeBottomText = (e) => {
-        console.log(e.target.value);
         this.setState({
             userBottomText: e.target.value
         })
@@ -54,7 +52,6 @@ class MemeCreator extends Component{
         })
     }
 
-
     hideCreateMemeSection = (e) => {
         if (e.target.id === "createMemeSection") {
             document.getElementById("createMemeSection").style.display = "none";
@@ -71,26 +68,22 @@ class MemeCreator extends Component{
         document.getElementById('searchGifId').style.display = 'flex';
     }
 
-
-   
-
     render(){
         return(
 
             <div className="createMemeSection" id="createMemeSection" onClick={this.hideCreateMemeSection}>
-    
-                
+
                 <div className="wrapperCreateMemePage">
                     <div className="buttonContainer">
                         <button className="closeMemeButton" onClick={this.closeMemeSection}>X</button>
                     </div>
-                     
+
                     <div className="gifAndForm">
 
                         <div className="selectedGif">
-                            <h5 className="memeText">{this.state.userTopText}</h5>
+                            <h5 className="memeText memeTextTop">{this.state.userTopText}</h5>
                             <img src={this.props.gifUrlProps} alt={this.props.gifAltProps} />
-                            <h5 className="memeText">{this.state.userBottomText}</h5>
+                            <h5 className="memeText memeTextBottom">{this.state.userBottomText}</h5>
                         </div>
 
                         <form action="" onSubmit={this.userGeneratedMemes} className="createMemeForm">
@@ -98,13 +91,13 @@ class MemeCreator extends Component{
                             <input className="srOnly" id="date" name="date" className="hiddenDate"></input>
 
                             <label htmlFor="topText">Top Text</label>
-                            <input type="text" id="topText" className="userInput" required placeholder="Top Text" ref={top => this.topText = top} onChange={this.handleInputChangeTopText} maxLength="" />
+                            <input type="text" id="topText" className="userInput" required placeholder="Top Text" maxLength="30" ref={top => this.topText = top} onChange={this.handleInputChangeTopText}/>
 
                             <label htmlFor="bottomText">Bottom Text</label>
-                            <input type="text" id="bottomText" className="userInput" required placeholder="Bottom Text" ref={bottom => this.bottomText = bottom} onChange={this.handleInputChangeBottomText} />
+                            <input type="text" id="bottomText" className="userInput" required placeholder="Bottom Text" maxLength="30" ref={bottom => this.bottomText = bottom} onChange={this.handleInputChangeBottomText}/>
 
                             <label htmlFor="tags">Tags</label>
-                            <input type="text" id="tags" className="userInput" required placeholder="Tags" ref={tags => this.tags = tags} />
+                            <input type="text" id="tags" className="userInput" required placeholder="Separate tags by commas or space" ref={tags => this.tags = tags} />
 
                             <input type="submit" className="submit" value="Submit!"></input>
                         </form>
@@ -112,8 +105,6 @@ class MemeCreator extends Component{
                     </div>
 
                 </div>
-                
-
 
             </div>
         )
