de00f3855658e1fcbb3a83a3a1c34305f306ae1e
HansChristian <hc.sev15@gmail.com>
2020-12-10 16:08:09 -0500

    Updated meme form.

--- src/components/TOWORKON.js
+++ src/components/TOWORKON.js
@@ -1,3 +1,12 @@
 // Get feedback when form is submitted (e.g. "meme created", where to view, create another (exits popup view)
 // Back buttons
 // Header explaining page (e.g. trending gifs, "  " gifs, most recent memes, " " memes)
+
+// npm install @fortawesome/free-solid-svg-icons
+// npm install @fortawesome/fontawesome-svg-core
+
+
+// 11: 24
+// @fortawesome/react-fontawesome
+// 11: 24
+// npm install @fortawesome/react-fontawesome
