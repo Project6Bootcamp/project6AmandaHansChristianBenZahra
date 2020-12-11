5522cd92a79e76f1b047763fb5f327f2c380d97c
SarjeantDev <bensarjeant@gmail.com>
2020-12-10 20:24:47 -0500

    updated some styling and a few bug fixes :bug:

--- src/components/SearchGifs.js
+++ src/components/SearchGifs.js
@@ -1,19 +1,20 @@
+// SearchGifs Class Component
+// Functionality: This component is used to get the results from a Giphy API call and allow the user to create a meme using one of the returned gifs.
+
 // importing npm installs
-import { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import axios from 'axios';
-
+import { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axios from "axios";
 // importing created pages & styles
-import MemeCreator from './MemeCreator.js';
-import Header from './Header.js';
-import '../styles/App.scss';
-
+import MemeCreator from "./MemeCreator.js";
+import Header from "./Header.js";
+import "../styles/App.scss";
 
 class SearchGifs extends Component {
     constructor() {
         super();
         this.state = {
-            userInput: '',
+            userInput: "",
             displayedGifs: [],
             gifUrl: [],
             gifAlt: [],
@@ -38,35 +39,35 @@ class SearchGifs extends Component {
         this.giphyAPICall(this.state.userInput);
     }
 
-
-
+    // api call to GIPHY that gets returns based off of a user search query (the userInput state), this api function gets fired off of the user submitting their search 
     giphyAPICall = (query) => {
         axios({
-            url: 'https://api.giphy.com/v1/gifs/search',
-            method: 'GET',
-            responseType: 'json',
+            url: "https://api.giphy.com/v1/gifs/search",
+            method: "GET",
+            responseType: "json",
             params: {
-                api_key: 'AiDUd8ngqnIcqZ5dXnGV8r4Aymleu4wa',
+                api_key: "AiDUd8ngqnIcqZ5dXnGV8r4Aymleu4wa",
                 q: query,
                 limit: 50,
-                rating: 'g'
+                rating: "g"
             }
         }).then((apiResponse) => {
-
+            // conditional statement to handle error of no gifs being found from query. errorMsgGifObj is a array of objects in the same structure as the return from the api call, creating this structure replica allows the map function in the render lifecycle to iterate with no problems
             if (apiResponse.data.data.length === 0) {
-                const errorMsg = [{
-                    id: '123ABC',
+                const errorMsgGifObj = [{
+                    id: "123ABC",
                     images: {
                         downsized_large: {
-                            url: 'https://media1.giphy.com/media/gfO3FcnL8ZK9wVgr6t/giphy.gif?…0f423ee6unrij1gzkfdf27jyfmh9v3oxbgftbq88ajrpq0&rid=giphy.gif'
+                            url: "https://media1.giphy.com/media/gfO3FcnL8ZK9wVgr6t/giphy.gif?…0f423ee6unrij1gzkfdf27jyfmh9v3oxbgftbq88ajrpq0&rid=giphy.gif"
                         }
                     }
                 }]
 
                 this.setState({
-                    displayedGifs: errorMsg
+                    displayedGifs: errorMsgGifObj
                 })
 
+            // else statement catching results from api call and storing result in the displayed Gifs state
             } else {
                 console.log(apiResponse.data.data);
                 this.setState({
@@ -75,102 +76,116 @@ class SearchGifs extends Component {
             }
         })
     }
+    // end of query based api call
 
 
+    // api call to GIPHY that returns the top 12 gifs that are trending from Giphy right now
     giphyTrendingAPICall = () => {
         axios({
-            url: 'https://api.giphy.com/v1/gifs/trending',
-            method: 'GET',
-            responseType: 'json',
+            url: "https://api.giphy.com/v1/gifs/trending",
+            method: "GET",
+            responseType: "json",
             params: {
-                api_key: 'AiDUd8ngqnIcqZ5dXnGV8r4Aymleu4wa',
+                api_key: "AiDUd8ngqnIcqZ5dXnGV8r4Aymleu4wa",
                 limit: 12,
-                rating: 'g'
+                rating: "g"
             }
         }).then((apiResponse) => {
+            // storing result in the displayedGif state
             this.setState({
                 displayedGifs: apiResponse.data.data
             })
         })
     }
+    // end of trending api call
 
+
+    // function which passes the url as a state to the MemeCreator component called in the render lifecycle. function also hides certain aspects of the page while displaying the create meme modal
     passUrl = (e) => {
-        this.setState({
-            gifUrl: e.target.src,
-            gifAlt: e.target.alt
-        })
-        document.getElementById('createMemeSection').style.display = 'flex';
-        document.getElementById("selectedGif").style.display = 'flex';
-        document.getElementById("createMemeForm").style.display =  'flex';
-        document.getElementById('displayedGifsId').style.marginTop = 0;
-        document.getElementById('root').style.backgroundColor = 'rgba(1, 1, 1, 0.9)';
-        document.getElementById('searchGifId').style.display = 'none';
-        document.getElementById('header').style.display = 'none';
-        document.getElementById('postCreationMeme').style.display = 'none';
+
+        // conditional statement to get the src of the gif regardless of if the user is clicking on the li containing the image or the image itself
+        if (e.target.firstChild != null){
+            console.log(e.target)
+            this.setState({          
+                gifUrl: e.target.firstChild.src,
+                gifAlt: e.target.firstChild.alt
+            })
+        } else {
+            this.setState({
+                gifUrl: e.target.src,
+                gifAlt: e.target.alt
+            })
+        }
+        
+        // displaying divs containing create meme page
+        document.getElementById("createMemeSection").style.display = "flex";
+        document.getElementById("selectedGif").style.display = "flex";
+        document.getElementById("createMemeForm").style.display =  "flex";
+        
+        // hiding search and navigation bar when create meme page pops up
+        document.getElementById("searchGifId").style.display = "none";
+        document.getElementById("header").style.display = "none";
+        document.getElementById("postCreationMeme").style.display = "none";
+        document.getElementById("displayedGifsId").style.marginTop = 0;
     }
 
 
+    // render statment loading content onto the page.
     render() {
         return (
             <Fragment>
                 <Header headerText="Meme in a Giffy" subheaderText="Search for Gifs to create your very own meme!" />
 
-                <section className="wrapper" id="wrapper">
+                {/* Calling the component MemeCreator to allow user to create their own Meme
+                - passes component the url and the alt text of the gif  
+                    **this is not visible until a user actually clicks a gif */}
+                <MemeCreator gifUrlProps={this.state.gifUrl} gifAltProps={this.state.gifAlt}/>
+
+                <section className="wrapper">
                  
-                    <div className="flexboxSearchGifs" id="searchGifId" >
-                    <form onSubmit={this.handleSubmit} className="flexbox searchGifForm" >
+                    {/* Div containing the search/navigation bar above displayed gifs */}
+                    <div className="flexboxSearchGifs" id="searchGifId">
+                        {/* div containing the search feature */}
+                        <form onSubmit={this.handleSubmit} className="flexbox searchGifForm">
+                            <label htmlFor="userGifSearch" className="srOnly">Search for Gif:</label>
+                            <input type="text" id="userGifSearch" name="userGifSearch" placeholder="Search Gifs" onChange={this.handleInputChange} required />
+                            <button className="glow-on-hover searchGifButton">Find Gif</button>
+                        </form>
 
-                        <label htmlFor="userGifSearch" className="srOnly">Search for Gif:</label>
-                        <input
-                            type="text"
-                            id="userGifSearch"
-                            name="userGifSearch"
-                            placeholder="Search Gifs"
-                            required
-                            onChange={this.handleInputChange}
-                        />
-                        <button >Find Gif</button>
-
-
-                    </form>
-                    <div className="viewMemesButtonContainer">
+                        {/* div containing button to navigate to meme search page */}
+                        <div className="viewMemesButtonContainer">
                             <Link to={"/search"}>
                                 <button className="glow-on-hover" type="button">View Created Memes</button>
                             </Link>
+                        </div>
                     </div>
-
-                 </div>
-
-
-                    <div>          
-                        <MemeCreator 
-                            gifUrlProps={this.state.gifUrl}
-                            gifAltProps={this.state.gifAlt}
-                        />
-
-                    </div>
-
+                
+                    {/* Ul containing all gifs */}
                     <ul className="gifs flexbox" id="displayedGifsId">
+                        
                         {
+                            // mapping over the state containing which is set from axios (api call)
                             this.state.displayedGifs.map((eachGif) => {
-                                // Conditional statement to check the id of the gif - if id of gif matches id for 'no results found' then return some additional text, else display all gifs with matching query
-                                if (eachGif.id === '123ABC') {
+
+                                // Conditional statement for error handling if no results are found after a user searches, adds additional text to the page that the regular render wouldn't display
+                                if (eachGif.id === "123ABC") {
                                     return (
-                                        <li className="gifContainer noPointer" key={eachGif.id} >
+                                        <li className="gifContainer noPointer" key={eachGif.id}>
                                             <p className="noGifFoundText">Sorry No GIFs found, search again!</p>
                                             <img src={eachGif.images.downsized_large.url} alt={eachGif.title}/>
                                         </li>
                                     )
                                 } else {
                                     return (
-                                        <li className="gifContainer normalPointer" key={eachGif.id} >
-                                            <img className="gifs" src={eachGif.images.downsized_large.url} alt={eachGif.title} onClick={this.passUrl} />
+                                        <li className="gifContainer normalPointer" key={eachGif.id} onClick={this.passUrl}>
+                                            <img src={eachGif.images.downsized_large.url} alt={eachGif.title}  />
                                         </li>
                                     )
                                 }
-  
                             })
+                            // end of map statement
                         }
+
                     </ul>
 
                 </section>
